5ba7ad663fe835090549cd2b7897173ba0c12b4b
RenatFaiz <renatf87@gmail.com>
2019-11-13 20:29:42 +0300

    Products added to table products

--- src/main/java/model/Product.java
+++ src/main/java/model/Product.java
@@ -22,6 +22,6 @@ public class Product {
                 ", category='" + category + '\'' +
                 ", quantity=" + quantity +
                 ", isAvailable=" + isAvailable +
-                ", price=" + price;
+                ", price=" + price + "\n";
     }
 }
